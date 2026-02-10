62b6b0dbf94b381aa1d98d1b09b3059b286a1968
diviganteng79 <diviganteng79@gmail.com>
2026-02-10 20:24:22 +0700

    Update dashboard.js

--- js/dashboard.js
+++ js/dashboard.js
@@ -1,14 +1,3 @@
-(async()=>{
- const kta=localStorage.getItem("login");
- if(!kta) location.href="index.html";
- const r=await fetch("data/kta.json");
- const d=await r.json();
- let h="<tr><th>No</th><th>Nama</th><th>Status</th><th>KTA</th></tr>";
- d.forEach((u,i)=>{
-  h+=`<tr><td>${i+1}</td><td>${u.nama}</td><td>${u.status}</td><td>${u.no_kta}</td></tr>`;
- });
- list.innerHTML=h;
-})();
 async function renderDashboard() {
   await seedUsersIfNeeded();
   const kta = localStorage.getItem("login");
