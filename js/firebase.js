90e08822d3ed26a3f33b3792a65b4edcbcee5fb8
diviganteng79 <diviganteng79@gmail.com>
2026-02-11 21:41:04 +0700

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -1,23 +1,54 @@
-<script type = "module" > 
-  // Impor fungsi-fungsi yang Anda butuhkan dari SDK yang Anda perlukan
-  import { initializeApp } from "https://www.gstatic.com/firebasejs/12.9.0/firebase-app.js" ; 
-  impor { getAnalytics } dari "https://www.gstatic.com/firebasejs/12.9.0/firebase-analytics.js" ; 
-  // TODO: Tambahkan SDK untuk produk Firebase yang ingin Anda gunakan
-  // https://firebase.google.com/docs/web/setup#available-libraries
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.9.0/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/12.9.0/firebase-auth.js";
+import {
+  getFirestore,
+  collection,
+  doc,
+  getDoc,
+  getDocs,
+  setDoc,
+  updateDoc,
+  deleteDoc,
+  query,
+  orderBy,
+  where,
+  onSnapshot,
+  serverTimestamp
+} from "https://www.gstatic.com/firebasejs/12.9.0/firebase-firestore.js";
+import { getAnalytics, isSupported } from "https://www.gstatic.com/firebasejs/12.9.0/firebase-analytics.js";
 
-  // Konfigurasi Firebase aplikasi web Anda
-  // Untuk Firebase JS SDK v7.20.0 dan yang lebih baru, measurementId bersifat opsional
-  const firebaseConfig = { 
-    apiKey : "AIzaSyAc06GR9kgHbfZ_8IBWeDWet__d7aE9wA4" , 
-    authDomain : "jsc-database-d49f2.firebaseapp.com" , 
-    ID proyek : "jsc-database-d49f2" , 
-    storageBucket : "jsc-database-d49f2.firebasestorage.app" , 
-    messagingSenderId : "494874117797" , 
-    ID aplikasi : "1:494874117797:web:f6eb3275c68a6a7b82ef4c" , 
-    ID pengukuran : "G-4DE2EYFQXH" 
-  };
+const firebaseConfig = {
+  apiKey: "AIzaSyAc06GR9kgHbfZ_8IBWeDWet__d7aE9wA4",
+  authDomain: "jsc-database-d49f2.firebaseapp.com",
+  projectId: "jsc-database-d49f2",
+  storageBucket: "jsc-database-d49f2.firebasestorage.app",
+  messagingSenderId: "494874117797",
+  appId: "1:494874117797:web:f6eb3275c68a6a7b82ef4c",
+  measurementId: "G-4DE2EYFQXH"
+};
 
-  // Inisialisasi Firebase
-  const app = initializeApp ( firebaseConfig );
-  const analytics = getAnalytics ( app );
-</script>
+export const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const db = getFirestore(app);
+
+export let analytics = null;
+isSupported()
+  .then((ok) => {
+    if (ok) analytics = getAnalytics(app);
+  })
+  .catch(() => {});
+
+export {
+  collection,
+  doc,
+  getDoc,
+  getDocs,
+  setDoc,
+  updateDoc,
+  deleteDoc,
+  query,
+  orderBy,
+  where,
+  onSnapshot,
+  serverTimestamp
+};
